5879ed8f0be516bea582a21f8d83eeca4026eb7e
1322harshd <harsh.nz@outlook.com>
2025-05-20 11:00:07 +1200

    product list half done

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('Home.html')
+    trending_orders = [
+        {'item': 'T-shirt', 'price': 10, 'image': 'tshirt.jfif','brand':'abibas'},
+        {'item': 'Mug', 'price': 7, 'image': 'mug.jfif','brand':'milton'},
+        {'item': 'Cap', 'price': 5, 'image': 'cap.jfif','brand':'yankees'}
+    ]
+    return render_template('Home.html', trending_orders=trending_orders)
 
 if __name__ == '__main__':
     app.run(debug=True)
