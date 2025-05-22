a0a90bfc2b27f382aaddff115ec503c2e3296604
1322harsh <270449050@yoobeestudent.ac.nz>
2025-05-23 11:41:59 +1200

    rearranging of files for classfication and follow dry coding

--- app.py
+++ app.py
@@ -5,19 +5,19 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     trending_orders = [
-        {'item': 'T-shirt', 'price': 10, 'image': 'tshirt.jfif','brand':'abibas'},
-        {'item': 'Mug', 'price': 7, 'image': 'mug.jfif','brand':'milton'},
-        {'item': 'Cap', 'price': 5, 'image': 'cap.jfif','brand':'yankees'}
+        {'item': 'T-shirt', 'price': 10, 'image': 'products/tshirt.jfif', 'brand': 'abibas'},
+        {'item': 'Mug', 'price': 7, 'image': 'products/mug.jfif', 'brand': 'milton'},
+        {'item': 'Cap', 'price': 5, 'image': 'products/cap.jfif', 'brand': 'yankees'}
     ]
     recent_orders = [
-        {'item': 'Bottle', 'price': 12, 'image': 'Unknown.jpeg','brand':'milton'},
-        {'item': 'Bag', 'price': 20, 'image': 'bag.jpeg','brand':'wildcraft'},
-        {'item': 'Shoes', 'price': 50, 'image': 'shoes.jpeg','brand':'nike'}
+        {'item': 'Bottle', 'price': 12, 'image': 'products/Unknown.jpeg', 'brand': 'milton'},
+        {'item': 'Bag', 'price': 20, 'image': 'products/bag.jpeg', 'brand': 'wildcraft'},
+        {'item': 'Shoes', 'price': 50, 'image': 'products/shoes.jpeg', 'brand': 'nike'}
     ]
     my_orders = [
-        {'item': 'Notebook', 'price': 3, 'image': 'notebook.jpeg','brand':'classmate'},
-        {'item': 'Pen', 'price': 1, 'image': 'pen.png','brand':'pilot'},
-        {'item': 'Watch', 'price': 100, 'image': 'watch.jpeg','brand':'casio'}
+        {'item': 'Notebook', 'price': 3, 'image': 'products/notebook.jpeg', 'brand': 'classmate'},
+        {'item': 'Pen', 'price': 1, 'image': 'products/pen.png', 'brand': 'pilot'},
+        {'item': 'Watch', 'price': 100, 'image': 'products/watch.jpeg', 'brand': 'casio'}
     ]
     return render_template(
         'Home.html',
@@ -25,6 +25,9 @@ def home():
         recent_orders=recent_orders,
         my_orders=my_orders
     )
+@app.route('/shopping_cart')
+def shopping_cart():
+    return render_template('shopping_cart.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
